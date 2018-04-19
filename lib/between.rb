f6a881d8b9aa22537875639ab8864d6d137c3690
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2018-04-19 15:09:00 +0200

    Between
    Funktion

---
+++ lib/between.rb
@@ -0,0 +1,10 @@
+def between(input1, input2, input3)
+    if input2 < input1
+        if input1 < input3
+            output = true
+        end
+    else
+        output = false
+    end
+    return output
+end
