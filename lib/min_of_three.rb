6ae0cb6be5a716d173103e393c07a24d5d9f6f2a
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2018-04-26 14:01:48 +0200

    Min of three
    Funktion

---
+++ lib/min_of_three.rb
@@ -0,0 +1,13 @@
+def min_of_three(input1, input2, input3)
+    output = input1
+    if input2 < input1
+        output = input2
+    end
+    if output > input3
+        output = input3
+    end
+
+    return output
+end
+
+puts min_of_three(-2, -10, 10)
