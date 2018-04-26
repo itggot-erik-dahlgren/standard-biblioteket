a0dcfad184adc74b6ab8585a8f660e8f33328c8b
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2018-04-26 14:53:54 +0200

    Power
    Funktion

---
+++ lib/power.rb
@@ -0,0 +1,14 @@
+# Public: Takes two numbers as inputs and gives the power as output, with first input as base and others as an exponent.
+#
+# input1  - The base
+# input2  - The exponent
+#
+# Examples
+#
+#   power(3)
+#   # => 15
+#
+# Returns the power
+def power(input1, input2)
+    return input1 ** input2
+end
