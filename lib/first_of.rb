b27b6be2e4d8e32b7ccb17c354444001e17f7465
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2018-04-27 11:45:52 +0200

    First of
    Funktion

---
+++ lib/first_of.rb
@@ -0,0 +1,13 @@
+# Public: Takes an array as input and gives the first number.
+#
+# array  - The array
+#
+# Examples
+#
+#   first_of([1, 123, 10])
+#   # => 1
+#
+# Returns the first number.
+def first_of(array)
+    return array[0]
+end
