ed6e367e1ec7b94487cb8a21a14c3c27474d2fe0
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2018-04-27 11:47:57 +0200

    Last of
    Funktion

---
+++ lib/last_of.rb
@@ -0,0 +1,15 @@
+# Public: Takes an array as input and gives the last number.
+#
+# array  - The array
+#
+# Examples
+#
+#   last_of([1, 123, -10])
+#   # => -10
+#
+# Returns the last number.
+def last_of(array)
+    return array[-1]
+end
+
+puts last_of([10, 123, -10])
